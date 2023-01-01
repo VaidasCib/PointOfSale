c93b34ab746cb38d9459175ac32c7ff97ed11a91
Vaidas Čibiras <55850111+siturez@users.noreply.github.com>
2023-01-01 19:41:27 +0200

    taxes hotfix

--- src/main/java/dropdatabase/pos/impl/payments/taxes/Taxes.java
+++ src/main/java/dropdatabase/pos/impl/payments/taxes/Taxes.java
@@ -3,18 +3,15 @@ package dropdatabase.pos.impl.payments.taxes;
 import dropdatabase.pos.common.BaseEntity;
 import lombok.Data;
 
-import javax.persistence.*;
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.math.BigDecimal;
-import java.util.Date;
-import javax.persistence.*;
 
 @Data
 @Entity
 @Table(name = "taxes")
-public class Taxes extends BaseEntity{
-    @Column(name = "total")
-    private String name;
+public class Taxes extends BaseEntity {
     @Column(name = "tax")
     private BigDecimal tax_percentage;
 }
